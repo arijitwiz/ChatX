c781375645484eea15127de22522d10d42cc50eb
Soumyajit2035 <soumyajitbhattacharjee.2004@gmail.com>
2025-04-28 04:27:22 +0530

    chnages

--- client/src/api.js
+++ client/src/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import { logout } from "./shared/utils/auth";
 
 const apiClient = axios.create({
-  // baseURL: "https://chatx-rqq5.onrender.com/api",
-  baseURL: "http://localhost:4444/api",
+  baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:4444/api", // Default to local if no environment variable
   timeout: 1000,
 });
 
